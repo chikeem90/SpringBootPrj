ce507145679c3f576ff28275b3833247291060c0
chikeem90 <chikeem90@gmail.com>
2020-10-05 23:36:08 +0900

    dev-tools까지

--- springtestdemo/src/test/java/io/chikeem90/springtestdemo/sample/SampleControllerTest.java
+++ springtestdemo/src/test/java/io/chikeem90/springtestdemo/sample/SampleControllerTest.java
@@ -1,13 +1,19 @@
 package io.chikeem90.springtestdemo.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,20 +24,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
-    @Autowired
-    TestRestTemplate testRestTemplate;
+    @Rule
+    public OutputCaptureRule outputCaptureRule = new OutputCaptureRule();
 
     @MockBean
     SampleService mockSampleService;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception {
         // SampleService를 모킹해서 빈을 교체함
         // getName에 chikeem90을 리턴하도록
         when(mockSampleService.getName()).thenReturn("chikeem90");
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello chikeem90");
+        mockMvc.perform(get("/hello")).andExpect(content().string("hello chikeem90"));
+        assertThat(outputCaptureRule.toString()).contains("holoman").contains("skip");
     }
 }
