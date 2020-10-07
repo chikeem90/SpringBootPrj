1e2f6f765257f513dbbdef0168e52d30af5108db
chikeem90 <chikeem90@gmail.com>
2020-10-07 10:51:57 +0900

    리소스까지

---
+++ demospringmvc/src/main/java/io/chikeem90/demospringmvc/user/UserController.java
@@ -0,0 +1,17 @@
+package io.chikeem90.demospringmvc.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
+    }
+}
