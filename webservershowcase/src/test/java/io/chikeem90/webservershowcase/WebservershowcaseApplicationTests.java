e823ab3ac27b7321f8fdd828e3acf9980f6fb91a
chikeem90 <chikeem90@gmail.com>
2020-10-14 09:32:26 +0900

    http, https, http2 예제

---
+++ webservershowcase/src/test/java/io/chikeem90/webservershowcase/WebservershowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chikeem90.webservershowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservershowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
